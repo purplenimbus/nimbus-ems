096886fbfa075f38435738eb6844d54fc2b922a8
purplenimbus <you@example.com>
2018-08-26 21:02:40 +0100

    update packages
    add npm audit suggestions

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -20,7 +20,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
@@ -28,6 +27,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/typeahead.js/dist/typeahead.bundle.js',
       'bower_components/angular-typeahead/dist/angular-typeahead.js',
       'bower_components/moment/moment.js',
